fc148f98b21a0a13eb1da417d0e50feadd5b6408
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-10-09 01:36:45 +0500

    Update forms-submission-script.js

--- forms-submission-script.js
+++ forms-submission-script.js
@@ -1,4 +1,9 @@
 const GetQuoteFormBtn = document.getElementById("show-quote-form-btn");
 const quoteFormMain = document.getElementById("quote-form-container");
+quoteFormMain.style.display = "none";
 
 console.log(GetQuoteFormBtn, quoteFormMain);
+GetQuoteFormBtn.addEventListener("click", () => {
+  GetQuoteFormBtn.style.display = "none";
+  quoteFormMain.style.display = "block";
+});
