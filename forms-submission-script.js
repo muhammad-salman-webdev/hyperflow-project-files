c1dd20b59e9f1346baf66765adfbd60bbcf7c42b
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-10-09 01:31:25 +0500

    Form Added

--- forms-submission-script.js
+++ forms-submission-script.js
@@ -1,1 +1,4 @@
-alert("hi");
+const GetQuoteFormBtn = document.getElementById("show-quote-form-btn");
+const quoteFormMain = document.getElementById("quote-form-container");
+
+console.log(GetQuoteFormBtn, quoteFormMain);
