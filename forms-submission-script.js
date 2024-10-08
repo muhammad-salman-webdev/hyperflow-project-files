9458270b917220d2a5f69bda81afdd59590ddf31
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-10-09 02:11:49 +0500

    Update forms-submission-script.js

--- forms-submission-script.js
+++ forms-submission-script.js
@@ -1,9 +1,16 @@
 const GetQuoteFormBtn = document.getElementById("show-quote-form-btn");
 const quoteFormMain = document.getElementById("quote-form-container");
 quoteFormMain.style.display = "none";
+const contactFormMain = document.getElementById("contact-form-hideOn");
+const formPrivacyContent = document.getElementById(
+  "from-pricvay-content-ShowOn"
+);
 
 console.log(GetQuoteFormBtn, quoteFormMain);
 GetQuoteFormBtn.addEventListener("click", () => {
   GetQuoteFormBtn.style.display = "none";
   quoteFormMain.style.display = "block";
+
+  if (contactFormMain) contactFormMain.style.display = "none";
+  if (formPrivacyContent) formPrivacyContent.style.display = "none";
 });
